6d51900603467d8f1e4119c27674d11cbc8ca7ae
tomohiro.mori <tomohiro.mori.hs@gmail.com>
2020-08-09 16:37:47 +0900

    Add Employee index

--- src/models/validators/EmployeeValidator.java
+++ src/models/validators/EmployeeValidator.java
@@ -45,7 +45,7 @@ public class EmployeeValidator {
         // すでに登録されている社員番号との重複チェック
         if(code_duplicate_check_flag){
             EntityManager em = DBUtil.createEntityManager();
-            long emploees_count = (long)em.createNamedQuery("checkRegisteredCode", long.class)
+            long emploees_count = (long)em.createNamedQuery("checkRegisteredCode", Long.class)
                     .setParameter("code", code)
                     .getSingleResult();
 
